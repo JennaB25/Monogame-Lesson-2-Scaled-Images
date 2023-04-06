f09ed105c8e46a271e9db580d4699c14ecd8a55b
362700239 <jennarbloor@gmail.com>
2023-04-06 10:51:24 -0400

    finished monogame part 2

--- Monogame Lesson 2 – Scaled Images/Game1.cs
+++ Monogame Lesson 2 – Scaled Images/Game1.cs
@@ -8,6 +8,13 @@ namespace Monogame_Lesson_2___Scaled_Images
     {
         Texture2D rectangleTexture;
         Texture2D circleTexture;
+        Rectangle rectangleRect;
+        Rectangle rectangleRect2;
+        Rectangle rectangleRect3;
+        Rectangle circleRect;
+        Rectangle circleRect2;      
+        private SpriteFont font;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -24,7 +31,14 @@ namespace Monogame_Lesson_2___Scaled_Images
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.ApplyChanges();
 
+            circleRect = new Rectangle(200, 350, 100, 100);
+            circleRect2 = new Rectangle(500, 350, 100, 100);
+            rectangleRect = new Rectangle(100, 300, 600, 100);
+            rectangleRect2 = new Rectangle(200, 200, 350, 100);
+            rectangleRect3 = new Rectangle(400, 210, 140, 80);
+
             base.Initialize();
+
         }
 
         protected override void LoadContent()
@@ -33,9 +47,9 @@ namespace Monogame_Lesson_2___Scaled_Images
 
             rectangleTexture = Content.Load<Texture2D>("rectangle");
             circleTexture = Content.Load<Texture2D>("circle");
+            font = Content.Load<SpriteFont>("Text");
 
         }
-
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -48,11 +62,22 @@ namespace Monogame_Lesson_2___Scaled_Images
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Bisque);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(rectangleTexture, rectangleRect, Color.DarkSlateGray);
+            _spriteBatch.Draw(rectangleTexture, rectangleRect2, Color.DarkSlateGray);
+            _spriteBatch.Draw(rectangleTexture, rectangleRect3, Color.LightBlue);
+            _spriteBatch.Draw(circleTexture, circleRect, Color.Black);
+            _spriteBatch.Draw(circleTexture, circleRect2, Color.Black);
+            _spriteBatch.DrawString(font, "Delorean", new Vector2(140, 310), Color.Black);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+
+
+
     }
 }
